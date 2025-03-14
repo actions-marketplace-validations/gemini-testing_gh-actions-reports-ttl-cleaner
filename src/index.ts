0cb708f04eb60f05a0ad2c809e0bccb69f710abb
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2025-03-14 04:46:58 +0300

    feat: main implementation

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import core from "@actions/core";
+
+import * as input from "./input.js";
+import { gitCommit, gitConfigureAuthor, gitPush } from "./git-utils.js";
+import { getNestedDirectories, removeDirectories } from "./fs-utils.js";
+import { getDateUTCStringBeforeDays, isStringDateBefore, looksLikeDate } from "./date-utils.js";
+
+async function main() {
+    const userName = input.getValidatedUserName();
+    const userEmail = input.getValidatedUserEmail();
+
+    await gitConfigureAuthor(userName, userEmail);
+
+    const reportsPrefix = await input.getValidatedReportPrefix();
+    const ttl = input.getValidatedTTL();
+
+    const removeOlderThanDate = getDateUTCStringBeforeDays(ttl);
+
+    core.debug(`Looking for reports older than ${removeOlderThanDate}`);
+
+    const nestedDirectories = await getNestedDirectories(reportsPrefix);
+    const nestedDateLikeDirectories = nestedDirectories.filter(looksLikeDate);
+    const nestedDirectoriesToRemove = nestedDateLikeDirectories.filter((directory) => {
+        return isStringDateBefore(directory, removeOlderThanDate);
+    });
+
+    if (!nestedDirectoriesToRemove.length) {
+        core.warning(
+            `Nothing to remove: ${nestedDateLikeDirectories.length} directories inside ${reportsPrefix} are up-to-date`,
+        );
+    } else {
+        console.debug(`Removing ${nestedDirectoriesToRemove.length} directories inside ${reportsPrefix}`);
+
+        await removeDirectories(reportsPrefix, nestedDirectoriesToRemove);
+
+        await gitCommit(reportsPrefix);
+
+        await gitPush();
+    }
+}
+
+main().catch((error) => core.setFailed(error));
